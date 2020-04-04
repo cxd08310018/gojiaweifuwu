c58f19ef309edd7f9368a011a5627ae2794d0b59
15633104375 <cxd0831>
2020-04-04 21:30:06 +0800

    测试001

---
+++ ceshi001/src/main/java/com/cxd/ceshi001/Ceshi001Application.java
@@ -0,0 +1,13 @@
+package com.cxd.ceshi001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceshi001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ceshi001Application.class, args);
+    }
+
+}
